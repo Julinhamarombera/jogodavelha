f41eadd6f87deb2fa1a97ca27a920dc752ded74b
Paiva <106314874+Julinhamarombera@users.noreply.github.com>
2023-11-24 10:24:48 -0300

    jogooo

--- jogo.js
+++ jogo.js
@@ -9,4 +9,20 @@ const JOGADOR_0 = "0";
 
 let Jogoacabou = false;
 
-document.addEventListener("click", (event) => {}
+document.addEventListener("click", (event) => {
+    if (event.target.matches(".celula") && !jogoacabou) {
+         jogar(event.target.id);
+         }
+        
+    }); function jogar(id) { 
+        const celula = document.getElementById(id); 
+        if (celula.textContent ==="") { 
+            turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+            checarTurno = !checarTurno;
+            celula.textContent = turno;
+            celula.classList.add(turno);
+            celula.removeEventListener("click", jogar);
+             verificarVencedor(turno);
+         }
+     }
+
